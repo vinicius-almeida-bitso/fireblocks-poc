5546589eb2258d275213654a6a44000f43392195
vinicius-almeida-bitso <vinicius.almeida@bitso.com>
2022-03-21 11:21:59 -0300

    setup project

---
+++ src/main/java/com/bitso/fireblockspoc/FireblocksPocApplication.java
@@ -0,0 +1,13 @@
+package com.bitso.fireblockspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireblocksPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireblocksPocApplication.class, args);
+	}
+
+}
